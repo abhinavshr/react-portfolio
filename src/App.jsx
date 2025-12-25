c23c12e909719d87fa44baa6bdf902ef6cfecda2
Abhinav Shrestha <139767616+abhinavshr@users.noreply.github.com>
2025-12-25 11:57:06 +0545

    refactor: the design of the admin login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AdminLogin from './pages/admins/AdminLogin';
+import HomePage from './pages/users/home';
 import './App.css';
 
 function App() {
@@ -8,8 +9,9 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Navigate to="/admin/login" replace />} />
+          <Route path="/" element={<Navigate to="/home" replace />} />
           
+          <Route path="/home" element={<HomePage />} />
           <Route path="/admin/login" element={<AdminLogin />} />
           
           <Route path="*" element={<Navigate to="/admin/login" replace />} />
