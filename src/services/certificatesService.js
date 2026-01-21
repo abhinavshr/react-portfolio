847bde90b7f6878fce02b176bcc26f613b0c4eef
Abhinav Shrestha <139767616+abhinavshr@users.noreply.github.com>
2026-01-21 19:57:24 +0545

    feat: added the pagination in the certificate page

--- src/services/certificatesService.js
+++ src/services/certificatesService.js
@@ -1,12 +1,12 @@
 import api from "./api";
 
-export const viewAllCertificates = async () => {
-    try {
-        const response = await api.get("/admin/certificates");
-        return response.data;
-    } catch (error) {
-        throw error.response?.data || { message: "Failed to fetch certificates" };
-    }
+export const viewAllCertificates = async (page = 1) => {
+  try {
+    const response = await api.get(`/admin/certificates?page=${page}`);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || { message: "Failed to fetch certificates" };
+  }
 };
 
 export const addCertificate = async (data) => {
